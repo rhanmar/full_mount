f1d9b31f9de76aaec5437ab27cc4dfec7de5c636
rhrhrh <dimontura@yandex.ru>
2022-11-01 16:56:45 +0700

    refactor(db): create nested schemas for list of Events and Fights

--- db/schemas.py
+++ db/schemas.py
@@ -1,54 +1,42 @@
 from pydantic import BaseModel
 
 
-class EventBase(BaseModel):
+class BaseSchema(BaseModel):
+
+    class Config:
+        orm_mode = True
+
+
+class EventBase(BaseSchema):
+    pass
+
+
+class FightBase(BaseSchema):
+    pass
+
+
+class FighterBase(BaseSchema):
+    pass
+
+
+class FighterInFight(FighterBase):
     id: int
     name: str
-    is_over: bool | None
-    date: str | None
-    location: str | None
-    url: str | None
-
-    class Config:
-        orm_mode = True
-
-
-class FightBase(BaseModel):
-    id: int
-    fighter1: str
-    fighter2: str
-    odds_fighter1: float | None
-    odds_fighter2: float | None
-    is_over: bool | None
-    event_id: int | None
-
-    class Config:
-        orm_mode = True
-
-
-class EventInFight(EventBase):
-    pass
+    country: str | None
 
 
 class FightInEvent(FightBase):
-    pass
-
-
-class FightRead(BaseModel):
     id: int
-    fighter1: str
-    fighter2: str
+    fighter1: FighterInFight
+    fighter2: FighterInFight
+    winner: FighterInFight | None
     odds_fighter1: float | None
     odds_fighter2: float | None
     is_over: bool | None
     event_id: int | None
-    event: EventInFight
-
-    class Config:
-        orm_mode = True
 
 
-class EventRead(BaseModel):
+class EventRead(EventBase):
     id: int
     name: str
     is_over: bool | None
@@ -57,5 +45,98 @@ class EventRead(BaseModel):
     url: str | None
     fights: list[FightInEvent] = None
 
-    class Config:
-        orm_mode = True
+###
+
+
+class EventInFight(EventBase):
+    id: int
+    name: str
+    is_over: bool | None
+    date: str | None
+    location: str | None
+    url: str | None
+
+
+class FightRead(FightBase):
+    id: int
+    fighter1: FighterInFight
+    fighter2: FighterInFight
+    winner: FighterInFight | None
+    odds_fighter1: float | None
+    odds_fighter2: float | None
+    is_over: bool | None
+    event_id: int | None
+    event: EventInFight
+
+
+
+# class EventBase(BaseModel):
+#     id: int
+#     name: str
+#     is_over: bool | None
+#     date: str | None
+#     location: str | None
+#     url: str | None
+#
+#     class Config:
+#         orm_mode = True
+#
+#
+# class FightBase(BaseModel):
+#     id: int
+#     odds_fighter1: float | None
+#     odds_fighter2: float | None
+#     is_over: bool | None
+#     event_id: int | None
+#
+#     class Config:
+#         orm_mode = True
+#
+#
+# class FighterBase(BaseModel):
+#     id: int
+#     name: str
+#     country: str | None
+#
+#     class Config:
+#         orm_mode = True
+#
+#
+# class EventInFight(EventBase):
+#     pass
+#
+#
+# class FightInEvent(FightBase):
+#     pass
+#
+#
+# class FighterInFight(FighterBase):
+#     pass
+#
+#
+# class FightRead(FightBase):
+#     id: int
+#     fighter1: FighterInFight
+#     fighter2: FighterInFight
+#     winner: FighterInFight | None
+#     odds_fighter1: float | None
+#     odds_fighter2: float | None
+#     is_over: bool | None
+#     event_id: int | None
+#     event: EventInFight
+#
+#     class Config:
+#         orm_mode = True
+#
+#
+# class EventRead(BaseModel):
+#     id: int
+#     name: str
+#     is_over: bool | None
+#     date: str | None
+#     location: str | None
+#     url: str | None
+#     fights: list[FightInEvent] = None
+#
+#     class Config:
+#         orm_mode = True
